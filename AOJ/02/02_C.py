728d6595c02d23c332a4b751b5f61ea7cff4ac4f
Airi2000 <aphoom2000@yahoo.co.jp>
2022-11-05 13:00:26 +0900

    add sort

--- AOJ/02/02_C.py
+++ AOJ/02/02_C.py
@@ -1,8 +1,3 @@
-a, b, c = map(int, input().split())
-if a > b:
-    a, b = b, a
-if b > c:
-    b, c = c, b
-if a > b:
-    a, b = b, a
-print(a, b, c)
+abc = list(map(int, input().split()))
+abc.sort()
+print(abc[0], abc[1], abc[2])
