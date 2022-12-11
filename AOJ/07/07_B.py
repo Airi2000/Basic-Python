88a2def93e9f264831bdb41e710314bec373dce3
Airi2000 <aphoom2000@yahoo.co.jp>
2022-12-11 22:22:13 +0900

    update1

--- AOJ/07/07_B.py
+++ AOJ/07/07_B.py
@@ -6,10 +6,8 @@ while True:
     if n == 0 and x == 0:
         break
     m = range(1, n+1)
-    c = (n*(n-1)*(n-2))//6
-    com = list(itertools.combinations(m, 3))
-    for i in range(c):
-        s = sum(com[i])
+    for i in list(itertools.combinations(m, 3)):
+        s = sum(i)
         if s == x:
             a += 1
     print(a)
